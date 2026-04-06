d38b4401ce2c0a6c271bc112720f7191236f1d30
Ghavvvo <2gaherre12@gmail.com>
2026-04-06 00:07:19 -0400

    feat : new base route 2

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/',
+  base: '/transbot-frontend/',
   server: {
     host: "::",
     port: 8080,
